434fc78f07ab056c9206e34abc6d5d4f832adfec
Brian Carlstrom <bdc@google.com>
2011-01-24 16:17:12 -0800

    jarjar org.bouncycastle to com.android.org.bouncycastle
    Repackaging org.bouncycastle.* into the private namespace
    com.android.org.bouncycastle.* to avoid collisions with applications
    bundling their own version of BouncyCastle.
    
    Bug: 3086427
    Change-Id: If3b02f9e9a3e2c9bf220a420de17d61932feb165

--- Android.mk
+++ Android.mk
@@ -24,6 +24,7 @@ LOCAL_SRC_FILES := $(bouncycastle_src_files)
 LOCAL_JAVACFLAGS := -encoding UTF-8
 LOCAL_JAVA_LIBRARIES := core
 LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 include $(BUILD_JAVA_LIBRARY)
 
 # This is used to generate a list of what is unused so it can be removed when bouncycastle is updated.
@@ -76,5 +77,6 @@ ifeq ($(WITH_HOST_DALVIK),true)
     LOCAL_NO_STANDARD_LIBRARIES := true
     LOCAL_BUILD_HOST_DEX := true
     LOCAL_MODULE_TAGS := optional
+    LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
     include $(BUILD_HOST_JAVA_LIBRARY)
 endif
